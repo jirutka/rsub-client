3942daafe9cc00e8cb49debfbcf556dbca5fcdd0
Jakub Jirutka <jakub@jirutka.cz>
2015-08-10 13:36:57 +0200

    Use single/double quotes more consistently

--- setup.py
+++ setup.py
@@ -14,19 +14,19 @@ try:
     from pypandoc import convert
     read_md = lambda f: convert(f, 'rst')
 except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    print('warning: pypandoc module not found, could not convert Markdown to RST')
     read_md = lambda f: open(f, 'r').read()
 
 setup(
-    name="rsub",
-    version="1.0.0",
-    url="https://github.com/jirutka/rsub-client",
-    description="Open and edit files from a remote machine in your local Sublime Text or TextMate 2.",
-    long_description=read_md("README.md"),
-    author="Jakub Jirutka",
-    author_email="jakub@jirutka.cz",
-    license="MIT",
-    scripts=["bin/rsub"],
+    name='rsub',
+    version='1.0.0',
+    url='https://github.com/jirutka/rsub-client',
+    description='Open and edit files from a remote machine in your local Sublime Text or TextMate 2.',
+    long_description=read_md('README.md'),
+    author='Jakub Jirutka',
+    author_email='jakub@jirutka.cz',
+    license='MIT',
+    scripts=['bin/rsub'],
     install_requires=['docopt>=0.5.0'],
     classifiers=[
         'Environment :: Console',
